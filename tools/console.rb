44e78f2bdafcb10de59d8ab677164003d0b993f1
lisantwi <lisaantwi@gmail.com>
2019-07-01 20:58:06 -0400

    complete

--- tools/console.rb
+++ tools/console.rb
@@ -1,9 +1,33 @@
+
 # You don't need to require any of the files in lib or pry.
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+lisa = Lifter.new("Lisa", 50)
+karl = Lifter.new("Karl", 100)
+chinee = Lifter.new("Chinee", 200)
+
+gold = Gym.new("Gold's Gym")
+onelife = Gym.new("One Life")
+wework = Gym.new("WeWork Gym")
+
+lisa.add_membership(20, gold)
+lisa.add_membership(30, wework)
+lisa.add_membership(30, onelife)
+karl.add_membership(30, onelife)
+
+lisa.get_list_memberships
+lisa.get_list_gyms
+
+Lifter.get_lift_average
+
+onelife.get_membership
+onelife.get_lifters
+
+
+
 
 binding.pry
+
 
 puts "Gains!"
