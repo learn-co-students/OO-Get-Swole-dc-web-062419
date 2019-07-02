b8c7043f2e62e510cc4624c7e87c6eb338a0db22
github username <github email address>
2019-07-02 08:02:16 -0400

    Done.

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,19 @@
 class Membership
   attr_reader :cost
+  attr_writer :MEMBERSHIPS
 
+  MEMBERSHIPS = []
   def initialize(cost)
     @cost = cost
+    MEMBERSHIPS << self 
   end
+
+  def self.all 
+    MEMBERSHIPS 
+  end 
+
+
+
+
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -2,7 +2,26 @@
 # We've done it for you here.
 require_relative '../config/environment.rb'
 
-# test code goes here
+kolton = Lifter.new("Kolton", 0)
+bobby = Lifter.new("Bobby", 0)
+sarah = Lifter.new("Sarah", 0)
+juan = Lifter.new("Juan", 0)
+
+la_fitness = Gym.new("LA Fitness")
+la_mem = Membership.new(50)
+golds = Gym.new("Golds Gym")
+golds_mem = Membership.new(100)
+orange_theory = Gym.new("Orange Theory Fitness")
+orange_mem = Membership.new(150)
+
+kolton.join_gym(la_fitness,la_mem)
+bobby.join_gym(golds, golds_mem)
+sarah.join_gym(orange_theory, orange_mem)
+juan.join_gym(golds, golds_mem)
+
+
+puts "Should return the lifter instances that have a memberships at the gym"
+golds.memberships
 
 binding.pry
 
