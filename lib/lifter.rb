cc04b84b40429a856af44f2d03ddeb0ee8f179c5
Luke O'Malley <lukeomalley95@gmail.com>
2019-07-07 02:46:17 +0000

    Done.

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,15 @@
 class Lifter
   attr_reader :name, :lift_total
+  
+  @@all = []
 
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+    self.class.all << self
+  end
+  
+  def self.all
+    @@all
   end
 end
